27417e8dbea66ec36d40592ae6cd7572cec28c50
sreerajchundayil <sreerajc.programming@gmail.com>
2021-05-23 13:26:52 +0530

    Added movement and Lock code

--- pieces.js
+++ pieces.js
@@ -1,4 +1,4 @@
-
+let stopAnimation = false;
 const PiecePatternI =
 [
   [
@@ -32,6 +32,35 @@ const PiecePatternI =
 
 ]
 
+const PiecePatternL =
+[
+  [
+    [
+      [1,0,0],
+      [1,0,0],
+      [1,1,0]
+    ],
+    [
+      [1,1,1],
+      [1,0,0],
+      [0,0,0]
+    ],
+    [
+      [0,1,1],
+      [0,0,1],
+      [0,0,1]
+    ],
+    [
+      [0,0,0],
+      [0,0,1],
+      [1,1,1]
+    ]
+  
+  ]
+  ,
+  ["RED"]
+
+]
 
 const PiecePatternT =
 [
@@ -65,20 +94,20 @@ const PiecePatternT =
 
 class Piece
 {
-  constructor(tetriminos, board)
+  constructor(board, tetriminos, type = 0)
   {
     this.board = board;
     this.tetrimino = tetriminos;
-    this.type = 0;
+    this.type = type;
     this.x = 0;
     this.y = 0;
-    console.log("Piece creation done");
+    this.locked = false;
+    this.dropStart = Date.now();
   }
   
   DrawPiece()
   {
     let color = this.tetrimino[1] ;
-    //console.log(piece.tetrimino[0][piece.type]);
     for(let r = 0; r < this.tetrimino[0][this.type].length; r++)
     {
       for(let c = 0; c < this.tetrimino[0][this.type].length; c++)
@@ -88,5 +117,135 @@ class Piece
       }
     }
   }
+
+  UnDrawPiece()
+  {
+    for(let r = 0; r < this.tetrimino[0][this.type].length; r++)
+    {
+      for(let c = 0; c < this.tetrimino[0][this.type].length; c++)
+      {
+          if(this.tetrimino[0][this.type][r][c] == 1)
+            this.board.DrawSqaure(this.x+c, this.y+r, "WHITE"); //x is from left, means it's the column and y is from up means the row
+      }
+    }
+  }
+  
+  MoveDown()
+  {
+    if(this.locked)
+      return;
+    if(this.CollisionDetection(0,1))
+    {
+      this.Lock();
+      return;
+    }
+    
+    this.UnDrawPiece();
+    this.y++;
+    this.DrawPiece();
+  }
+  
+  MoveLeft()
+  {
+    if(this.locked)
+      return;
+    if(this.CollisionDetection(-1,0))
+    {
+      return;
+    }
+    
+    this.UnDrawPiece();
+    this.x--;
+    this.DrawPiece();
+  }
+  
+  MoveRight()
+  {
+    if(this.locked)
+      return;
+    if(this.CollisionDetection(1,0))
+    {
+      return;
+    }
+    this.UnDrawPiece();
+    this.x++;
+    this.DrawPiece();
+  }
+
+  Rotate()
+  {
+    if(this.locked)
+      return;
+    let toBeTetrimino = new Piece(this.board, this.tetrimino, (this.type+1)%4);
+    if(toBeTetrimino.CollisionDetection(0,0))
+    {
+      return;
+    }
+    this.UnDrawPiece();
+    this.type = (this.type+1)%4;
+    this.DrawPiece();
+  }
+
+  
+  Drop()
+  {
+    let now = Date.now();
+    let delta = now - this.dropStart;
+    if(delta > 1000)
+    {
+      this.MoveDown();
+      this.dropStart = now;
+    }
+
+    if(this.y >= (this.board.rowSize - this.tetrimino[0][this.type].length))
+      return; 
+    if(stopAnimation == false)
+      requestAnimationFrame(this.Drop.bind(this)); //requestAnimationFrame(()=>this.loop());
+    else
+      return;
+  }
+
+  CollisionDetection(x,y)
+  {
+    for(let r = 0 ; r < this.tetrimino[0][this.type].length; r++)
+    {
+      for(let c = 0 ; c < this.tetrimino[0][this.type].length; c++)
+      {
+        if(!this.tetrimino[0][this.type][r][c])
+          continue;
+        let newX = this.x + c + x;
+        let newY = this.y + r + y;
+
+        if(newX < 0 || newX >= this.board.colSize || newY >= this.board.rowSize)
+          return true;
+
+        if(newY < 0)
+          continue;
+
+        if(this.board.board[newY][newX] != "white")
+          return true;
+      }
+    }
+    return false;
+  }
+
+  Lock()
+  {
+    this.locked = true;
+    let color = this.tetrimino[1];
+    //Give board piece Color
+    for(let r = 0; r < this.tetrimino[0][this.type].length; r++)
+    {
+      for(let c = 0; c < this.tetrimino[0][this.type].length; c++)
+      {
+          if(this.tetrimino[0][this.type][r][c] == 1)
+          {
+            this.board.DrawSqaure(this.x+c, this.y+r, color); //x is from left, means it's the column and y is from up means the row
+          }
+      }
+    }
+    stopAnimation = true; 
+  }
+
 }
 
--- tetris.js
+++ tetris.js
@@ -2,13 +2,34 @@ const cvs = document.getElementById("tetris");
 const ctx = cvs.getContext("2d");
 
 
-const board  = new Board(400,80,"white",40)
+
+const board  = new Board(800,600,"white",40)
 board.DrawGridOnBoard();
 
-let piece1 = new Piece(PiecePatternT, board);
-let piece2 = new Piece(PiecePatternI, board);
-piece1.DrawPiece();
-piece2.DrawPiece();
+let piece1 = new Piece(board, PiecePatternL);
+let piece2 = new Piece(board, PiecePatternI);
 
 
+piece1.Drop();
 
+document.addEventListener("keydown",CONTROL);
+
+function CONTROL(event)
+{
+  if(event.keyCode == 37)
+  {
+    piece1.MoveLeft();
+  }
+  else if(event.keyCode == 38)
+  {
+    piece1.Rotate();
+  }
+  else if(event.keyCode == 39)
+  {
+    piece1.MoveRight();
+  }
+  else if(event.keyCode == 40)
+  {
+    piece1.MoveDown();
+  }
+}
